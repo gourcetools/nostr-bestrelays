0790b3647dcdf1a6cabeebd94153e99ff8470bb7
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-01-21 05:29:09 +0100

    filesnames

--- nostr-bestrelays.sh
+++ nostr-bestrelays.sh
@@ -11,18 +11,13 @@ echo " "
 echo "   📡 https://github.com/gourcetools/nostr-bestrelays "
 echo "          Ping and find best nostr relays for you "
 echo " "
-
 # Delete a potentially old relays-list.txt
 rm -f relays-list.txt
 # Download a list of nostr relays from nostr.watch
-
-
-echo " "
 echo "  == 🌐 Downloading full relays list from nostr.watch "
-# Download a list of nostr relays from nostr.watch
 wget -q https://raw.githubusercontent.com/dskvr/nostr-watch/develop/relays.yaml
 echo " "
-
+# Check if file is here	
 if [ -s relays.yaml ]
 then
 echo "  == ✅ Succes.  "
@@ -33,11 +28,6 @@ echo "  == ❌ Download failed, restarting...  "
 ./nostr-bestrelays.sh
 fi
 
-
-echo " "
-echo "  ======================================================= "
-echo " "
-
 # Remove the first line wich is " Relays :"
 sed -i "1d" relays.yaml
 
@@ -46,19 +36,20 @@ cat relays.yaml | sed 's/^.\{10\}//' > urllist.txt
 
 # Delete relays.yaml, we dont need it anymore.
 rm -f relays.yaml
-# Delete any relays that have a / , because it breaks the ping command 
-sed -i '/\//d' urllist.txt
+# Delete any relays that have a / , if it breaks the ping command 
+# sed -i '/\//d' urllist.txt
 
-
-
+# urllist.txt is ready for pinging
+echo " "
+echo "  ======================================================= "
+echo " "
 # Ping the urls in urllist.txt and sort them by ping
-
 cat urllist.txt | while read LINE
 do
     echo "  == 🏓 Pinging $LINE "
     # Timeout 0.5 second. If it takes more than that, we dont want this relay.
     # Output a list starting with pings in sorted.txt
-    timeout 0.5 ping -c 1 $LINE | tail -n 1 | awk '{print $4}' | cut -d '/' -f 2 | echo $(cat) + "$LINE" >> pinged.txt
+    timeout 0.5 ping -c 1 "$LINE" | tail -n 1 | awk '{print $4}' | cut -d '/' -f 2 | echo $(cat) + "$LINE" >> pinged.txt
 done
 echo " "
 echo " "
@@ -66,44 +57,45 @@ echo " "
 echo "   ✅ Done pinging all relays.  "
 echo " "
 
-#kick relays that errored or returned no ping before sorting#
+# Delete urllist.txt.txt we dont need it anymore.
+rm -f urllist.txt
 
+#Delete relays that errored or returned no ping before sorting
 while read line; do
   if [[ $line =~ ^[0-9] ]]; then
-    echo "$line" >> temp.txt
+    echo "$line" >> ok.txt
   fi
 done < pinged.txt
-
-mv temp.txt pinged.txt
-
+mv ok.txt pinged.txt
 
 
 #Sort relays by ping
-sort -n pinged.txt > ipsorted.txt
+sort -n pinged.txt > sorted.txt
 
-# Delete sorted.txt, we dont need it anymore.
+# Delete pinged.txt, we dont need it anymore.
 rm pinged.txt
 
-# Remove pings from relay list.
+# Remove pings data from sorted relay list.
 while read line; do
-	echo ${line} | sed 's/.*+//'>>under1srelays.txt
-done <ipsorted.txt
+	echo ${line} | sed 's/.*+//'>>URLrelays.txt
+done <sorted.txt
+# Delete sorted.txt, we dont need it anymore.
+rm -f sorted.txt
 
-# Delete ipsorted.txt, we dont need it anymore.
-rm -f ipsorted.txt
 
 # Remove first character wich is a dot left from the pinging... it works.
-sed -i 's/^.//' under1srelays.txt
+sed -i 's/^.//' URLrelays.txt
 
 # Keep the first 10 lines
-head -n 10 under1srelays.txt > relays-list.txt
+head -n 10 URLrelays.txt > relays-list.txt
 
-# Delete thoses, we dont need them anymore
-rm -f under1srelays.txt
-rm -f urllist.txt
+# Delete URLrelays.txt we dont need it anymore.
+rm -f URLrelays.txt
 
 # Add back wss:// to relays list
 sed -i 's/^/wss:\/\//' relays-list.txt
+
+# All done :)
 echo "================================================== "
 echo "👇 👇 👇 👇   Best relays for you 👇 👇 👇 👇 "
 echo " "
@@ -114,6 +106,6 @@ echo "================================================== "
 echo " "
 echo "  =========================================="
 echo "  💾 Saved 10 best relays in: "
-echo "       📁 => nostr-bestrelays/relays-list.txt "
+echo "       📁 => ./relays-list.txt "
 echo "  🙏 Thank you for using nostr-bestrelays. "
 echo "  =========================================="
