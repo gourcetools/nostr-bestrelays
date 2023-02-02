03ed1c8d3ce857efd5b8a5b06c5042dedcdc299e
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-02-02 11:05:12 +0100

    Use API. | Timeout 0.250 instead of 0.5

--- nostr-bestrelays.sh
+++ nostr-bestrelays.sh
@@ -13,11 +13,11 @@ echo " "
 # Delete a potentially old relays-list.txt
 rm -f relays-list.txt
 # Download a list of nostr relays from nostr.watch
-echo "  == 🌐 Downloading full relays list from nostr.watch "
-wget -q https://raw.githubusercontent.com/dskvr/nostr-watch/develop/relays.yaml
+echo "  == 🌐 Downloading online relays list from nostr.watch "
+curl -s https://api.nostr.watch/v1/online | jq -r '.[]' > relays.txt
 echo " "
 # Check if file is here	
-if [ -s relays.yaml ]
+if [ -s relays.txt ]
 then
 echo "  == ✅ Succes.  "
 else
@@ -27,15 +27,12 @@ echo "  == ❌ Download failed, restarting...  "
 ./nostr-bestrelays.sh
 fi
 
-# Remove the first line wich is " Relays :"
-sed -i "1d" relays.yaml
-
 # Remove spaces and wss:// from relays so we can ping urls
 
-cut -c 11- relays.yaml > urllist.txt && rm -f relays.yaml
+cut -c 7- relays.txt > urllist.txt && rm -f relays.txt
 
 
-# Delete any relays that have a / , if it breaks the ping command 
+# Delete any relays that have a / , cuz it breaks the ping command...
 sed -i '/\//d' urllist.txt
 
 
@@ -47,9 +44,9 @@ echo " "
 cat urllist.txt | while read LINE
 do
     echo "  == 🏓 Pinging $LINE "
-    # Timeout 0.5 second. If it takes more than that, we dont want this relay.
+    # Timeout 0.250 second. If it takes more than that, we dont want this relay.
     # Output a list starting with pings in sorted.txt
-    timeout 0.5 ping -c 1 "$LINE" | tail -n 1 | awk '{print $4}' | cut -d '/' -f 2 | echo $(cat) + "$LINE" >> pinged.txt
+    timeout 0.250 ping -c 1 "$LINE" | tail -n 1 | awk '{print $4}' | cut -d '/' -f 2 | echo $(cat) + "$LINE" >> pinged.txt
     sed -n '$s/\(.*\)+.*/\1/p' pinged.txt
 done
 echo " "
